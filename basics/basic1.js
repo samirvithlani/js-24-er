4fe7ef2dcc1519af5aa8ee58d3837ea9a947f6ba
samirvithlani <samir.vithlani83955@gmail.com>
2024-06-21 12:56:36 +0530

    basics

---
+++ basics/basic1.js
@@ -0,0 +1,32 @@
+//log
+console.log("Hello World!")
+//warn
+console.warn("This is a warning!")
+//error
+console.error("This is an error!")
+
+
+var a = 10;
+console.log(a)
+console.log("value of  a = " , a)
+console.log("type of a = ",typeof a)
+
+var per = 78.90
+console.log("per = ",per)
+console.log("type of per = ",typeof per)
+
+var userName = "raj"
+console.log("userName=  ",userName)
+console.log("type of userName = ",typeof userName)
+
+var num1 = 100
+var num2 = 200
+
+var sum = num1 + num2
+console.log("sum = ",sum)
+
+
+
+
+
+
