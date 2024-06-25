d2d539c56d056cd4fa1d573a0eb7da02333124d9
samirvithlani <samir.vithlani83955@gmail.com>
2024-06-25 14:59:04 +0530

    if else

---
+++ ifelse/equalop.js
@@ -0,0 +1,11 @@
+// == , ===
+//== only checks value not data type..
+var no1 = 10
+var no2 = "10"
+
+if(no1 === no2){
+    console.log("Equal")
+}
+else{
+    console.log("Not Equal")
+}
---
+++ ifelse/ifelse1.js
@@ -0,0 +1,27 @@
+var no1 = -100
+
+if(no1>0){
+    console.log("Positive")
+}
+else{
+    console.log("Negative")
+}
+
+var x =20
+
+if(x % 2 ==0){
+    console.log("Even")
+}
+else{
+    console.log("Odd")
+}
+
+var p =20
+var q =20
+
+if(p>=q){
+    console.log("P is greater")
+}
+else{
+    console.log("Q is greater")
+}
