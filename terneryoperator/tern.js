aae679ab6b0a2aef689ee09747dbf4f50be7837b
samirvithlani <samir.vithlani83955@gmail.com>
2024-07-12 12:49:32 +0530

    ternary

---
+++ terneryoperator/tern.js
@@ -0,0 +1,63 @@
+var no1 = 10
+var no2 = 20
+
+if(no1>no2){
+    console.log("No1 is greater")
+}
+else{
+    console.log("No2 is greater")
+}
+
+// Ternery Operator ? :
+no1 > no2 ? console.log("no1 is greater"): console.log("no2 is greater")
+
+var x = 91
+if(x %2 ==0){
+    console.log("Even")
+}
+else{
+    console.log("Odd")
+}
+x %2 ==0 ? console.log("even") : console.log("odd")
+x>0 ? console.log("positive"):console.log("negative")
+
+
+//var age = parseInt(prompt("Enter your age"))
+
+//age>18 ? console.log("you can vote") : console.log("you can't vote")
+
+var no =21
+
+no % 3 ==0 && no % 9 ==0 ? console.log("Divisible by 3 and 9") : console.log("Not divisible by 3 and 9")    
+
+var x1 = 10
+var x2 = "10"
+
+
+x1 == x2 ? console.log("Equal") : console.log("Not Equal")
+x1 === x2 ? console.log("Equal") : console.log("Not Equal")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
