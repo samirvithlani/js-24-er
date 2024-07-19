d70663aedd04a62a49ccb0952ec4d04e3fe7f63b
samirvithlani <samir.vithlani83955@gmail.com>
2024-07-19 12:46:08 +0530

    char i

---
+++ string/string4.js
@@ -0,0 +1,12 @@
+//count...
+
+var data = "hi this is india"
+var count =0
+for(let i=0;i<data.length;i++){
+
+ //   console.log(data[i])
+    if(data[i]=="h"){
+        count++
+    }
+}
+console.log("count of i = ",count)
