c2c5199953d711a2b036398f11cbb51dba7aa8a1
samirvithlani <samir.vithlani83955@gmail.com>
2024-07-19 12:53:41 +0530

    string task

--- string/string4.js
+++ string/string4.js
@@ -1,12 +1,38 @@
 //count...
 
-var data = "hi this is india"
-var count =0
-for(let i=0;i<data.length;i++){
+// var data = "hi this is india"
+// var count =0
+// for(let i=0;i<data.length;i++){
 
- //   console.log(data[i])
-    if(data[i]=="h"){
-        count++
+//  //   console.log(data[i])
+//     if(data[i]=="h"){
+//         count++
+//     }
+// }
+// console.log("count of i = ",count)
+
+// var data = "hi this is india"
+// var count =0
+// for(let i=0;i<data.length;i++){
+
+//  //   console.log(data[i])
+//     if(data[i]==" "){
+//         count++
+//     }
+// }
+// console.log("count of i = ",count+1)
+
+const countWords = (data) => {
+  //var data = "hi this is india";
+  var count = 0;
+  for (let i = 0; i < data.length; i++) {
+    //   console.log(data[i])
+    if (data[i] == " ") {
+      count++;
     }
-}
-console.log("count of i = ",count)
+  }
+  return count+1;
+};
+
+var cnt = countWords("Hi this is India")
+console.log("Count = ",cnt)
