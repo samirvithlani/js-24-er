38938c5c0a344e0ce63b91f289c9aec68808e773
samirvithlani <samir.vithlani83955@gmail.com>
2024-07-26 12:23:54 +0530

    json

---
+++ json/json1.js
@@ -0,0 +1,45 @@
+var sname = "raj"
+var semail = "raj@gmail.com"
+var sage = 23
+// ..
+// ..
+// ..
+
+var student ={
+    name:"raj",
+    email:"raj@gmail.com",
+    age:23,
+    address:{
+    city:"bangalore",
+    state:"karnataka",
+    pincode:560068,
+    street:"7th cross",
+    }
+}
+
+console.log(student)
+console.log(student.name)
+console.log(student.email)
+console.log(student.age)
+
+// console.log(student.city)
+// console.log(student.state)
+// console.log(student.pincode)
+// console.log(student.street)
+console.log(student.address)
+console.log(student.address.city)
+console.log(student.address.state)
+console.log(student.address.pincode)
+console.log(student.address.street)
+
+
+var employee ={
+    id:101,
+    name:"amit",
+    qualification:{
+        degree:"BE",
+        percentage:65,
+    }
+}
+
+console.log(employee)
