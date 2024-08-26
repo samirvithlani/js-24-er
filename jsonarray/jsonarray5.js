9a0ec4186a53136ffd03aa043c08e1481d93fe68
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-26 14:09:53 +0530

    department

---
+++ jsonarray/jsonarray5.js
@@ -0,0 +1,79 @@
+var departments = [
+    {
+        name:"IT",
+        employees:[
+            {
+                name:"amit",
+                age:23,
+                gender:"male",
+                isActive:true
+            },
+            {
+                name:"sumit",
+                age:25,
+                gender:"male",
+                isActive:true
+            },
+            {
+                name:"seeta",
+                age:26,
+                gender:"female",
+                isActive:true
+            }
+        ]
+    },
+    {
+        name:"HR",
+        employees:[
+            
+            {
+                name:"geeta",
+                age:32,
+                gender:"female",
+                isActive:true
+            },
+            {
+                name:"harita",
+                age:36,
+                gender:"female",
+                isActive:false
+            },
+        ]
+    },
+    {
+        name:"Finance",
+        employees:[
+            
+            {
+                name:"ajay",
+                age:35,
+                gender:"male",
+                isActive:true
+            },
+            {
+                name:"axit",
+                age:37,
+                gender:"male",
+                isActive:true
+            },
+            {
+                name:"parth",
+                age:39,
+                gender:"male",
+                isActive:true
+            },
+            {
+                name:"sunit",
+                age:42,
+                gender:"male",
+                isActive:false
+            }
+            
+        ]
+    },
+]
+
+//print all department names
+//find female employee from finance department
+//find male and active employee from it department
+//find all inactive employee from it department
