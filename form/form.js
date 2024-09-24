d4d071f91d7484564ac16e466617706ed4d7bfdb
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-24 14:43:53 +0530

    m

--- form/form.js
+++ form/form.js
@@ -4,9 +4,11 @@ const submitHandler =(event)=>{
     const name = document.getElementById("name")//<input type="text" id="name">
     const email = document.getElementById("email")//<input type="email" id="email">
     const country = document.getElementById("country")//<select id="country
-    console.log("name = ",name.value);
-    console.log("email = ",email.value);
-    console.log("country = ",country.value);
+    const color = document.getElementById("color")//<input type="color" id="color">
+    //console.log("color = ",color.value);
+    //console.log("name = ",name.value);
+   // console.log("email = ",email.value);
+    //console.log("country = ",country.value);
 
 
     const gender = document.getElementsByName("gender")//inputfemale intputmale
@@ -19,6 +21,24 @@ const submitHandler =(event)=>{
 
 
     }
+
+
+    const nameresult = document.getElementById("nameResult")
+    nameresult.innerHTML = name.value;
+    const emailresult = document.getElementById("emailResult")
+    emailresult.innerHTML = email.value;
+    const countryresult = document.getElementById("countryResult")
+    countryresult.innerHTML = country.value
+
+
+    const result = document.getElementById("result")//<div>
+    result.style.display = "block";//display:block
+    //result.style.backgroundColor = "lightgrey";
+    result.style.backgroundColor = color.value;
+    result.style.padding = "10px";
+    result.style.margin = "10px";
+    result.style.border = "1px solid black";
+
     
 
 
