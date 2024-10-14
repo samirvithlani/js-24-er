4a91bf69cfc25b22e27ce13a6c1763d439d2458f
samirvithlani <samir.vithlani83955@gmail.com>
2024-10-14 14:42:18 +0530

    event listners

--- eventlistners/eventlistner1.js
+++ eventlistners/eventlistner1.js
@@ -12,3 +12,33 @@ button.addEventListener("mouseout",function(){
  button.addEventListener("dblclick",function(){
     button.style.backgroundColor = "yellow"
  })
+
+ const link = document.getElementById("link")
+ link.addEventListener("click",function(){
+    link.target = "_blank"
+    link.href = "https://www.abc.com"
+ })
+
+
+ const box = document.getElementById("box")
+ box.addEventListener("click",()=>{
+    box.style.backgroundColor = "red"
+ })
+ box.addEventListener("mouseover",()=>{
+    box.style.height="400px"
+    box.style.width="400px"
+    const image = document.getElementById("image")
+    image.style.height="400px"
+    image.style.width="400px"
+    image.src = "./abc.png"
+    
+ })
+ box.addEventListener("mouseleave",()=>{
+    box.style.height="200px"
+    box.style.width="200px"
+    const image = document.getElementById("image")
+    image.style.height="200px"
+    image.style.width="200px"
+    image.src = ""
+    
+ })
