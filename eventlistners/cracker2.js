e1ea04fb6bfb7c31504a58f0c32c6ece8f0255e5
samirvithlani <samir.vithlani83955@gmail.com>
2024-10-29 14:32:12 +0530

    updated script

--- eventlistners/cracker2.js
+++ eventlistners/cracker2.js
@@ -1,48 +1,75 @@
-const ball =document.getElementById('ball1');
-let isDragging = false;
-let offsetX = 0;
-let offsetY = 0;
-
-ball.addEventListener("mousedown",(event)=>{
-
-    isDragging = true;
-    offsetX = event.clientX - ball.getBoundingClientRect().left;
-    console.log(offsetX)
-    offsetY = event.clientY - ball.getBoundingClientRect().top;
-    console.log(offsetY)
-    ball.style.cursor = "grabbing";
-})
-
-
-ball.addEventListener("mousemove",(event)=>{
-
-    if(isDragging){
-        console.log("x..",event.clientX)
-            const xPos = event.clientX - offsetX;
-            //772 -41 = 731
-            console.log(xPos)
-            const yPos = event.clientY - offsetY;
-            //778 -32 = 746
-            console.log("y..",event.clientY)
-            console.log(yPos)
-            ball.style.left   = xPos + "px"; //731
-            ball.style.top = yPos + "px"; //746
-            if(xPos>=60 && yPos>=51){
-                alert("clash")
-            }
-    }
-
-})
-
-const ball2 =document.getElementById('ball2');
+const ball1 = document.getElementById('ball1');
+const ball2 = document.getElementById('ball2');
+let isDragging1 = false;
 let isDragging2 = false;
-let offsetX2 = 0;
-let offsetY2 = 0;
+let offsetX1 = 0, offsetY1 = 0;
+let offsetX2 = 0, offsetY2 = 0;
 
-ball2.addEventListener("mousedown",(event)=>{
+// Function to check collision between two elements
+function checkCollision() {
+    const rect1 = ball1.getBoundingClientRect();
+    const rect2 = ball2.getBoundingClientRect();
+    
+    return !(
+        rect1.right < rect2.left ||
+        rect1.left > rect2.right ||
+        rect1.bottom < rect2.top ||
+        rect1.top > rect2.bottom
+    );
+}
 
-    offsetX = event.clientX - ball2.getBoundingClientRect().left;
-    console.log(offsetX)
-    offsetY = event.clientY - ball2.getBoundingClientRect().top;
-    console.log(offsetY)
-})
+// Ball1 event listeners
+ball1.addEventListener("mousedown", (event) => {
+    isDragging1 = true;
+    offsetX1 = event.clientX - ball1.getBoundingClientRect().left;
+    offsetY1 = event.clientY - ball1.getBoundingClientRect().top;
+    ball1.style.cursor = "grabbing";
+});
+
+document.addEventListener("mousemove", (event) => {
+    if (isDragging1) {
+        const xPos = event.clientX - offsetX1;
+        const yPos = event.clientY - offsetY1;
+        ball1.style.left = xPos + "px";
+        ball1.style.top = yPos + "px";
+        
+        if (checkCollision()) {
+            alert("Crash detected!");
+            isDragging1 = false;
+            isDragging2 = false;
+        }
+    }
+});
+
+document.addEventListener("mouseup", () => {
+    isDragging1 = false;
+    ball1.style.cursor = "grab";
+});
+
+// Ball2 event listeners
+ball2.addEventListener("mousedown", (event) => {
+    isDragging2 = true;
+    offsetX2 = event.clientX - ball2.getBoundingClientRect().left;
+    offsetY2 = event.clientY - ball2.getBoundingClientRect().top;
+    ball2.style.cursor = "grabbing";
+});
+
+document.addEventListener("mousemove", (event) => {
+    if (isDragging2) {
+        const xPos = event.clientX - offsetX2;
+        const yPos = event.clientY - offsetY2;
+        ball2.style.left = xPos + "px";
+        ball2.style.top = yPos + "px";
+        
+        if (checkCollision()) {
+            alert("Crash detected!");
+            isDragging1 = false;
+            isDragging2 = false;
+        }
+    }
+});
+
+document.addEventListener("mouseup", () => {
+    isDragging2 = false;
+    ball2.style.cursor = "grab";
+});
