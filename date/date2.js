775fc37a818f34154c579ed69205443e2fb53add
samirvithlani <samir.vithlani83955@gmail.com>
2024-11-11 14:57:50 +0530

    date

---
+++ date/date2.js
@@ -0,0 +1,14 @@
+//setter method
+
+var date = new Date(); //creation of date object
+console.log(date); //printing the date object
+date.setFullYear(2007); //setting the year
+date.setMonth(6); //setting the month
+date.setDate(7); //setting the date
+date.setHours(18); //setting the hours
+date.setMinutes(0); //setting the minutes
+date.setSeconds(0); //setting the seconds
+date.setMilliseconds(0); //setting the milliseconds
+
+
+console.log(date); //printing the date object
