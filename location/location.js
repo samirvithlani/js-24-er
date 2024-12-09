8cc3a36958db51bc66f71623008b3269a8fa1553
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-09 14:39:40 +0530

    location

--- location/location.js
+++ location/location.js
@@ -10,3 +10,20 @@ const getLocation =()=>{
 
 }
 getLocation()
+
+const getLiveLocation = () =>{
+
+
+    navigator.geolocation.watchPosition((postion)=>{
+        console.log(postion)
+        console.log("lat",postion.coords.latitude)
+        console.log("long.",postion.coords.longitude)
+
+    })
+
+}
+getLiveLocation()
+
+
+//distance calulator.. : curren lat long... swaswtik -->
+//freeze calulator..   : current 10sec : i m frreeze
